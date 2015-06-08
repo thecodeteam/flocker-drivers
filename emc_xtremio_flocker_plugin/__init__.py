7d4f1026e99f796e6bc4f34cee9964cbe5efb654
Vaibhav Khanduja <vaibhav.khanduja@emc.com>
2015-06-08 10:32:58 -0700

    added plugin dir

---
+++ emc_xtremio_flocker_plugin/__init__.py
@@ -0,0 +1,11 @@
+from flocker.node import BackendDescription, DeployerType
+from emc_xtremio_flocker_plugin.emc_xtremio_blockdevice import EMCXtremIOBlockDeviceAPI
+
+def api_factory(cluster_id, **kwargs):
+    return EMCXtremIOBlockDeviceAPI(cluster_id=cluster_id, xms_user=kwargs[u"xms_user"], xms_pass=kwargs[u"password"],
+					xms_ip=kwargs['xms_ip'])
+
+FLOCKER_BACKEND = BackendDescription(
+    name=u"emc_xtremio_flocker_plugin", # name isn't actually used for 3rd party plugins
+    needs_reactor=False, needs_cluster_id=True,
+    api_factory=api_factory, deployer_type=DeployerType.block)
